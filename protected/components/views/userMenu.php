2ec1f78966d3e37c6793a40bc86ec2529935a8d9
unknown <amalash@OCDT70261714.office.adroot.bmogc.net>
2013-06-17 16:05:30 -0400

    Fixed delete menu item

--- protected/components/views/userMenu.php
+++ protected/components/views/userMenu.php
@@ -1,7 +1,7 @@
 <div id="right-column"> <!-- sidebar -->
 <?php foreach (Yii::app()->controller->menu as $label) { ?>
 <div class = "column span-10 append-3 colborder">
-	<p><?php echo CHtml::link($label["label"], $label["url"]);?></p>
+	<p><?php echo CHtml::link($label["label"], $label["url"], array_key_exists("linkOptions",$label) ? $label["linkOptions"] : '') ;?></p>
 </div>
 <?php } ?>
 
