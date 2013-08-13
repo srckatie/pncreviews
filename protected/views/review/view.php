6cab0a05c532de8710a4fb7cbd011522680f92d1
unknown <amalash@OCDT70261714.office.adroot.bmogc.net>
2013-08-13 11:24:56 -0400

    Added test data; renamed Update review; Added image

--- protected/views/review/view.php
+++ protected/views/review/view.php
@@ -10,9 +10,9 @@ $this->breadcrumbs=array(
 $this->menu=array(
 	array('label'=>'List Review', 'url'=>array('index')),
 	array('label'=>'Create Review', 'url'=>array('create')),
-	array('label'=>'Update Review', 'url'=>array('update', 'id'=>$model->ReviewID)),
+	array('label'=>'Edit Review', 'url'=>array('update', 'id'=>$model->ReviewID)),
 	array('label'=>'Delete Review', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->ReviewID),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Review', 'url'=>array('admin')),
+	array('label'=>'Manage Reviews', 'url'=>array('admin')),
 );
 ?>
 
